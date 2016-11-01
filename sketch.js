a91cc2a87bed44bb02c416e7ab6bdfd1c53b2051
Bwackv <brick.shoemaker@gmail.com>
2016-11-01 18:09:42 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,57 @@
-function setup(){
-  createCanvas(600,400);
+var bubble = {
+  x: 300,
+  y: 200,
+  display: function() {
+    stroke(255);
+    strokeWeight(4);
+    fill(random(200,255));
+    ellipse(this.x, this.y, 24, 24);
+  },
+  move: function() {
+    this.x = this.x + random(-1, 1);
+    this.y = this.y + random(-1, 1);
+  }
+}
+
+var bubble2 = {
+  x: 200,
+  y: 200,
+  display: function() {
+    stroke(255);
+    strokeWeight(4);
+    fill(random(100,150));
+    ellipse(this.x, this.y, 24, 24);
+  },
+  move: function() {
+    this.x = this.x + random(-1, 1);
+    this.y = this.y + random(-1, 1);
+  }
+}
+var bubble3 = {
+  x: 250,
+  y: 250,
+  display: function() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, 200, 200);
+  },
+  move: function() {
+    this.x = this.x + random(-1, 1);
+    this.y = this.y + random(-1, 1);
+  }
+}
+function setup() {
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(85);
-  var diameter = random();
-  lollipop(100,100,50);
-  lollipop(200,100,50);
- 
-}
-
-function lollipop(x, y, diameter){
-   fill(0, 200, 255);
-  rect(x-10,y,20,150);
-  
-  fill(255,0,200);
-  ellipse(x, y, diameter, diameter);
+  background(0);
+  bubble.move();
+  bubble.display();
+  bubble2.move();
+  bubble2.display();
+  bubble3.move();
+  bubble3.display();
+  arc(250, 250, 80, 80, 0, PI);
 }
